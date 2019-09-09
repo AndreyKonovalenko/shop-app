69a704e29f8c443da3c2eaf0ef1eed5c986a3174
EC2 Default User <andreinew@mail.ru>
2019-09-09 12:43:57 +0000

    Setup store and dummy-data for fake db

---
+++ src/screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import {} from 'redux';
+import {} from 'react-redux';
+
+const ProductsOverviewScreen = props => {
+    return <FlatList />
+
+}
+
+export default ProductsOverviewScreen;
